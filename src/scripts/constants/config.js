1b7bc771f4573024990b31613001943245d4c40b
Pavel Maslov <pavel.masslove@gmail.com>
2020-09-03 17:10:47 +0300

    Изменен адрес сервера API

--- src/scripts/constants/config.js
+++ src/scripts/constants/config.js
@@ -1,6 +1,7 @@
 // адрес API новостей
 // const serverNewsUrl = process.env.NODE_ENV === 'development' ? 'https://newsapi.org/v2/everything' : 'https://newsapi.org/v2/everything';
-const serverNewsUrl = process.env.NODE_ENV === 'development' ? 'https://praktikum.tk/news/v2/everything' : 'https://praktikum.tk/news/v2/everything';
+// const serverNewsUrl = process.env.NODE_ENV === 'development' ? 'https://praktikum.tk/news/v2/everything' : 'https://praktikum.tk/news/v2/everything';
+const serverNewsUrl = process.env.NODE_ENV === 'development' ? 'https://nomoreparties.co/news/v2/everything' : 'https://nomoreparties.co/news/v2/everything';
 const serverApiUrl = process.env.NODE_ENV === 'development' ? 'https://api.0911.ru' : 'https://api.0911.ru';
 
 // текущая дата и -7 дней от нее
